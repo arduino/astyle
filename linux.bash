b7b86b9ad6f715813d86a99095b8cc1a37cb4d2f
Federico Fissore <federico@fissore.org>
2013-10-14 16:21:32 +0200

    setting JAVAINCS directly

--- linux.bash
+++ linux.bash
@@ -1,16 +1,20 @@
 #!/bin/bash -e
 
+if [ "x$JAVAINCS" == "x" ] ; then
+	export JAVAINCS="-I/usr/lib/jvm/default-java/include -I/usr/lib/jvm/default-java/include/linux"
+fi
+
 cd astyle-code/AStyle/build/gcc/
 
 make clean
 
-ARCH=linux CFLAGS=-m64 LDFLAGS=-m64 make java
+CFLAGS="-m64" LDFLAGS="-m64" make java
 
 cp bin/libastylej.so ../../../../libastylej64.so
 
 make clean
 
-ARCH=linux CFLAGS=-m32 LDFLAGS=-m32 make java
+CFLAGS="-m32" LDFLAGS="-m32" make java
 
 cp bin/libastylej.so ../../../../libastylej32.so
 
