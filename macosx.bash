36c88a06ab15b8018c43e2db98f0a3f5d16d1148
Federico Fissore <federico@fissore.org>
2013-10-14 16:40:49 +0200

    macosx

---
+++ macosx.bash
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+if [ "x$JAVA_HOME" == "x" ] ; then
+	export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/"
+fi
+
+cd astyle-code/AStyle/build/mac/
+
+make clean
+
+CFLAGS="-arch i386 -arch x86_64" LDFLAGS="-arch i386 -arch x86_64" make java
+
+cp bin/libastylej.dylib ../../../../
+
