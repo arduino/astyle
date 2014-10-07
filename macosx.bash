527f3746c3aa1c7b5b8406bcc805dff354a21bba
Federico Fissore <federico@fissore.org>
2014-10-07 11:29:21 +0200

    macosx: adding LDFLAGS=-liconv as per https://sourceforge.net/p/astyle/bugs/307

--- macosx.bash
+++ macosx.bash
@@ -8,7 +8,7 @@ cd astyle-code/AStyle/build/mac/
 
 make clean
 
-CFLAGS="-arch i386 -arch x86_64 -Os" LDFLAGS="-arch i386 -arch x86_64" make java
+CFLAGS="-arch i386 -arch x86_64 -Os" LDFLAGS="-arch i386 -arch x86_64 -liconv" make java
 
 cp bin/libastylej.dylib ../../../../libastylej.jnilib
 
