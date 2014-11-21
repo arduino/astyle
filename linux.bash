88e778f02d176d31b5c907f49a3ea73186388022
Federico Fissore <federico@fissore.org>
2014-11-21 10:57:35 +0100

    Upgrading to astyle 2.05

--- linux.bash
+++ linux.bash
@@ -6,11 +6,11 @@ make clean
 
 CFLAGS="-m64 -Os" LDFLAGS="-m64 -s" make java
 
-cp bin/libastylej.so ../../../../libastylej64.so
+cp bin/libastyle*.so ../../../../libastylej64.so
 
 make clean
 
 CFLAGS="-m32 -Os" LDFLAGS="-m32 -s" make java
 
-cp bin/libastylej.so ../../../../libastylej32.so
+cp bin/libastyle*.so ../../../../libastylej32.so
 
--- macosx.bash
+++ macosx.bash
@@ -1,14 +1,10 @@
 #!/bin/bash -e
 
-if [ "x$JAVA_HOME" == "x" ] ; then
-	export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/"
-fi
-
 cd astyle-code/AStyle/build/mac/
 
 make clean
 
 CFLAGS="-arch i386 -arch x86_64 -Os" LDFLAGS="-arch i386 -arch x86_64 -liconv" make java
 
-cp bin/libastylej.dylib ../../../../libastylej.jnilib
+cp bin/libastyle*.dylib ../../../../libastylej.jnilib
 
