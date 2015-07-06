351df3174b2391d9ec7f70d2a2a75c6987299aad
Federico Fissore <federico@fissore.org>
2015-07-06 11:01:50 +0200

    Upgrading to astyle 2.05.1

--- setup.bash
+++ setup.bash
@@ -2,7 +2,7 @@
 
 rm -rf astyle-code
 
-svn co svn://svn.code.sf.net/p/astyle/code/tags/2.05 astyle-code
+svn co svn://svn.code.sf.net/p/astyle/code/tags/2.05.1 astyle-code
 
 cd astyle-code
 for f in `ls ../patches/*.patch` ; do
